8dc365e401f0d027059c2b02db28235d39f80df9
Mauro <mhdelmonde@gmail.com>
2023-11-28 03:52:27 -0400

    03 recursion exercises

---
+++ 03_recursion/02_greet.js
@@ -0,0 +1,16 @@
+const greet2 = (name) => {
+  console.log(`How are you ${name}?`);
+};
+
+const bye = () => {
+  console.log(`Ok bye!`);
+};
+
+const greet = (name) => {
+  console.log(`Hello, ${name}!`);
+  greet2(name);
+  console.log(`Getting ready to say bye...`);
+  bye();
+};
+
+greet("Mauro");
